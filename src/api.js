37415804eb97d730ac9353a6878545ce650d0437
Andree L <andree.lr28@gmail.com>
2022-03-05 14:54:56 -0500

    Dinamic routes

--- src/api.js
+++ src/api.js
@@ -1,11 +1,48 @@
 const url = 'https://api.coincap.io/v2'
 
 function getAssets() {
-    return fetch(`${url}/assets?limit=20`)
+    return fetch(`${url}/assets?limit=20`, {
+        mode: 'cors',
+        headers: {
+            Origin:'*'
+        }
+    })
         .then(res => res.json())
         .then(res => res.data)
 }
 
+function getAsset(coin) {
+    return fetch(`${url}/assets/${coin}`, {
+        mode: 'cors',
+        method: 'GET',
+        headers: {
+            //Origin:'*'
+        }
+    })
+    .then(res => res.json())
+    .then(res => res.data)
+}
+
+function getAssetHistory(coin) {
+    const now = new Date()
+    const end = now.getTime()
+    now.setDate(now.getDate()-1)
+    const start = now.getTime()
+    
+
+
+    return fetch(`${url}/assets/${coin}/history?interval=h1&start=${start}&end=${end}`, {
+        mode: 'cors',
+        method: 'GET',
+        headers: {
+            //'Access-Control-Allow-Origin': '*',
+            //'Access-Control-Allow-Headers': 'Origin'
+        }
+    })
+    .then(res => res.json())
+    .then(res => res.data)
+}
+
 export default {
-    getAssets
+    getAssets, getAsset, getAssetHistory
 }
--- src/router.js
+++ src/router.js
@@ -3,10 +3,12 @@ import { createRouter, createWebHistory } from 'vue-router'
 import Home from "./views/Home"
 import About from "./views/About"
 import ErrorPage from "./views/Error"
+import CoinDetail from "./views/CoinDetail"
 
 const routes = [
     {path: '/', name:'Home', component: Home },
     {path: '/about', name:'About', component: About },
+    {path: '/coin/:id', name:'CoinDetail', component: CoinDetail },
     {path: '/:catchAll(.*)', name:'Error', component: ErrorPage } 
 ]
 
